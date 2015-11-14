321cec63b6a5bf6a569d8e925740d61b8be1adf2
binux <roy@binux.me>
2015-11-14 17:12:57 +0000

    fix     InterfaceError: Can not reconnect to MySQL after 1 attempt(s): Unread result found.

--- db/basedb.py
+++ db/basedb.py
@@ -27,6 +27,8 @@ class BaseDB(object):
 
     @property
     def dbcur(self):
+        if self.conn.unread_result:
+            self.conn.get_rows()
         self.conn.ping(reconnect=True)
         return self.conn.cursor()
 
